bac098588f77aa3fb49aab965d937421e01ae29b
Jeremy Muhia <jmuhia@etsy.com>
2019-09-25 20:33:30 -0400

    MILESTONE-4 inject schedulers

--- controller/src/main/java/com/veganafro/controller/implementation/MainActivityPresenter.kt
+++ controller/src/main/java/com/veganafro/controller/implementation/MainActivityPresenter.kt
@@ -6,32 +6,49 @@ import com.veganafro.controller.generic.GenericPresenter
 import com.veganafro.controller.generic.GenericView
 import com.veganafro.model.NytTopic
 import com.veganafro.networking.nyt.NytService
-import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.Scheduler
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
+import javax.inject.Named
 
 class MainActivityPresenter @Inject constructor() :
     GenericPresenter {
 
     private var view: GenericView? = null
+    @field:[Inject Named("mainScheduler")] lateinit var mainScheduler: Scheduler
+    @field:[Inject Named("backgroundScheduler")] lateinit var backgroundScheduler: Scheduler
+
     @Inject lateinit var nytMostShared: NytService
     @Inject lateinit var subscriptions: CompositeDisposable
 
+    constructor(view: GenericView, mainScheduler: Scheduler, backgroundScheduler: Scheduler)
+            : this() {
+        this.view = view
+        this.mainScheduler = mainScheduler
+        this.backgroundScheduler = backgroundScheduler
+    }
+
     override fun loadData() {
+        view?.onFetchDataStarted()
         subscriptions.clear()
 
         val subscription: Disposable = nytMostShared
             .mostShared(1, BuildConfig.NYT_CONSUMER_KEY)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(backgroundScheduler)
+            .observeOn(mainScheduler)
             .subscribe(
                 { nytTopic: NytTopic? ->
                     Log.v("MainActivityPresenter", "success: ${nytTopic?.results}")
+                    view?.onFetchDataSuccess(nytTopic?.results)
                 },
                 { error: Throwable? ->
                     Log.v("MainActivityPresenter", "failure: ${error?.message}")
+                    view?.onFetchDataError(error!!)
+                },
+                {
+                    Log.v("MainActivityPresenter", "completed")
+                    view?.onFetchDataCompleted()
                 }
             )
 
@@ -50,7 +67,7 @@ class MainActivityPresenter @Inject constructor() :
         this.view = null
     }
 
-    fun setView(genericView: GenericView) {
-        view?.let {} ?: run { view = genericView }
+    fun setView(view: GenericView) {
+        this.view = view
     }
 }
--- injector/src/main/java/com/veganafro/injector/RxModule.kt
+++ injector/src/main/java/com/veganafro/injector/RxModule.kt
@@ -2,7 +2,12 @@ package com.veganafro.injector
 
 import dagger.Module
 import dagger.Provides
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 class RxModule {
@@ -10,5 +15,18 @@ class RxModule {
     @Provides
     fun provideCompositeDisposable(): CompositeDisposable {
         return CompositeDisposable()
+    }
+
+    @Provides
+    @Singleton
+    @Named("mainScheduler")
+    fun provideMainScheduler(): Scheduler {
+        return AndroidSchedulers.mainThread()
+    }
+
+    @Provides
+    @Named("backgroundScheduler")
+    fun provideBackgroundScheduler(): Scheduler {
+        return Schedulers.io()
     }
 }
